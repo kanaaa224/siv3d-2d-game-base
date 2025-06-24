61268b8d6eb5d7b1d1b78339d32910562b7eb12a
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-06-24 10:47:22 +0900

    [ new ] main.cpp リネーム

---
+++ source/Main.cpp
@@ -0,0 +1,39 @@
+﻿# include "Common.hpp"
+# include "Scenes/Title.hpp"
+# include "Scenes/Game.hpp"
+
+void AssetsRegistration()
+{
+	FontAsset::Register(U"Title", FontMethod::MSDF, 48, U"example/font/RocknRoll/RocknRollOne-Regular.ttf");
+	FontAsset::Register(U"Bold",  FontMethod::MSDF, 48, Typeface::Bold);
+
+	FontAsset(U"Title").setBufferThickness(4);
+
+	TextureAsset::Register({ U"Player",             { U"Player" } }, U"example/svg/cat.svg");
+	TextureAsset::Register({ U"Stage 1 Background", { U"Stage"  } }, U"example/texture/ground.jpg");
+}
+
+void Initialize()
+{
+	Window::Resize(1280, 720);
+
+	Window::SetTitle(U"タイトル");
+}
+
+void Main()
+{
+	AssetsRegistration();
+
+	Initialize();
+
+	SM sceneManager;
+
+	sceneManager.add<Title>(SceneState::Title);
+	sceneManager.add<Game> (SceneState::Game);
+	sceneManager.init      (SceneState::Title, 0.5s);
+
+	while (System::Update())
+	{
+		if (not sceneManager.update()) break;
+	}
+}
